4c30b7c852cf6965ba9ab4f64a278a34ff05ee2b
BhatNishanthGanesh <nishanthbhat18@gmail.com>
2023-07-09 12:40:48 +0530

    Modified

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 // document.getElementById("yo").innerHTML="hii";
 // document.getElementsByClassName("yo").innerHTML="hii";
 
-var elements = document.getElementsByClassName("yo");
-for (var i = 0; i < elements.length; i++) {
-  elements[i].innerHTML = "hii";
-}
+// var elements = document.getElementsByClassName("yo");
+// for (var i = 0; i < elements.length; i++) {
+//   elements[i].innerHTML = "hii";
+// }
